ee5529f04c1491adf2a6030f7b60310284a94d69
Ulsa <ulsa@ulsa.com>
2019-11-19 13:56:22 -0700

    Videos

--- video/ViewController.swift
+++ video/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webVideo: WKWebView!
+    @IBAction func doTapBoton(_ sender: Any) {
+        let YoutubeID =  "78wE4evw_zs" // Your Youtube ID here
+        let appURL = NSURL(string: "youtube://www.youtube.com/watch?v=\(YoutubeID)")!
+        let webURL = NSURL(string: "https://www.youtube.com/watch?v=\(YoutubeID)")!
+        let application = UIApplication.shared
+        
+        if application.canOpenURL(appURL as URL) {
+            application.open(appURL as URL)
+        } else {
+            // if Youtube app is not installed, open URL inside Safari
+            application.open(webURL as URL)
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let cadena = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/78wE4evw_zs\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
+        webVideo.loadHTMLString(cadena, baseURL: nil)
     }
 
 
